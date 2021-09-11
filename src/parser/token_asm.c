2eea3e0aa41a80f144cfce5e52ff3025b5a8d967
jmartini <jacopomartini89@gmail.com>
2021-09-11 20:38:37 +0200

    slight safety

--- src/parser/token_asm.c
+++ src/parser/token_asm.c
@@ -5,7 +5,6 @@ static void	ft_token_translation_env(t_token *tkn)
 	//
 }
 
-/* !!! FREE tkn->quotes !!! */
 static void	ft_token_translation_quotes(t_token *tkn)
 {
 	int	len;
@@ -15,8 +14,8 @@ static void	ft_token_translation_quotes(t_token *tkn)
 		len = -1;
 		while (tkn->quotes[++len])
 			ft_printf("QTS DEBUG ASM\t: %d\t%p\n", len, tkn->quotes[len]);
-		free (tkn->quotes);
 	}
+	free (tkn->quotes);
 }
 
 void	ft_token_assembler(t_token *tkn)
@@ -24,10 +23,12 @@ void	ft_token_assembler(t_token *tkn)
 	char	*output;
 	int		len;
 
+	output = NULL;
 	ft_token_translation_quotes(tkn);
 	len = tkn->end - tkn->start + 1;
 	output = ft_calloc(len + 1, sizeof(*output));
 	ft_memcpy(output, tkn->start, len);
 	ft_printf("ARGS_ASSEMBLER : %s\n", output);
 	free (output);
+	output = NULL;
 }
