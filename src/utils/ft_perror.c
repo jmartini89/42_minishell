e05ec61d43a280a06aa877780574c508bcb2528f
Jacopo Martini <jacopomartini89@gmail.com>
2021-09-13 23:47:13 +0200

    ft_perror malloc debug

--- src/utils/ft_perror.c
+++ src/utils/ft_perror.c
@@ -5,5 +5,7 @@ int	ft_perror(int err)
 	ft_putstr_fd(M_SHELL_NAME" : ", 2);
 	ft_putstr_fd((char *)g_errstr[err], 2);
 	ft_putstr_fd("\n", 2);
+	if (err == ERR_SYS_MALLOC)
+		return (-1);
 	return (0);
 }
