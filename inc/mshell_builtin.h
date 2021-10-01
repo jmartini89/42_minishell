3e4d1ae33c4c5b3fbeca11bad5be68a9ccaf4a8d
jmartini <jacopomartini89@gmail.com>
2021-10-02 00:33:56 +0200

    builtin : echo

--- inc/mshell_builtin.h
+++ inc/mshell_builtin.h
@@ -12,7 +12,8 @@ enum	e_builtin {
 	BLTIN_UNSET,
 };
 
-int		ft_builtin(t_shell *shell, char **arg);
+int		ft_builtin(t_shell *shell, char **argv);
+void	ft_echo(t_shell *shell, char **argv);
 void	ft_env(t_shell *shell, char **argv);
 void	ft_exec(t_shell *shell);
 void	ft_exit(t_shell *shell);
--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -1,44 +1,44 @@
 #include "minishell.h"
 
-static void	ft_builtin_launch(t_shell *shell, char **arg, int status)
+static void	ft_builtin_launch(t_shell *shell, char **argv, int status)
 {
 	if (status == (BLTIN_CD))
 		;
 	if (status == (BLTIN_ECHO))
-		;
+		ft_echo(shell, argv);
 	if (status == (BLTIN_ENV))
-		ft_env(shell, arg);
+		ft_env(shell, argv);
 	if (status == (BLTIN_EXIT))
 		ft_exit(shell);
 	if (status == (BLTIN_EXEC))
 		ft_exec(shell);
 	if (status == (BLTIN_EXPORT))
-		ft_export(shell, arg);
+		ft_export(shell, argv);
 	if (status == (BLTIN_PWD))
 		;
 	if (status == (BLTIN_UNSET))
 		;
 }
 
-int	ft_builtin(t_shell *shell, char **arg)
+int	ft_builtin(t_shell *shell, char **argv)
 {
 	int	status;
 
 	status = 0;
-	if (!ft_memcmp(arg[0], "cd", 2) && ft_strlen(arg[0]) == 2)
+	if (!ft_memcmp(argv[0], "cd", 2) && ft_strlen(argv[0]) == 2)
 		status = (BLTIN_CD);
-	if (!ft_memcmp(arg[0], "echo", 4) && ft_strlen(arg[0]) == 4)
+	if (!ft_memcmp(argv[0], "echo", 4) && ft_strlen(argv[0]) == 4)
 		status = (BLTIN_ECHO);
-	if (!ft_memcmp(arg[0], "env", 3) && ft_strlen(arg[0]) == 3)
+	if (!ft_memcmp(argv[0], "env", 3) && ft_strlen(argv[0]) == 3)
 		status = (BLTIN_ENV);
-	if (!ft_memcmp(arg[0], "exit", 4) && ft_strlen(arg[0]) == 4)
+	if (!ft_memcmp(argv[0], "exit", 4) && ft_strlen(argv[0]) == 4)
 		status = (BLTIN_EXIT);
-	if (!ft_memcmp(arg[0], "export", 6) && ft_strlen(arg[0]) == 6)
+	if (!ft_memcmp(argv[0], "export", 6) && ft_strlen(argv[0]) == 6)
 		status = (BLTIN_EXPORT);
-	if (!ft_memcmp(arg[0], "pwd", 3) && ft_strlen(arg[0]) == 3)
+	if (!ft_memcmp(argv[0], "pwd", 3) && ft_strlen(argv[0]) == 3)
 		status = (BLTIN_PWD);
-	if (!ft_memcmp(arg[0], "unset", 5) && ft_strlen(arg[0]) == 5)
+	if (!ft_memcmp(argv[0], "unset", 5) && ft_strlen(argv[0]) == 5)
 		status = (BLTIN_UNSET);
-	ft_builtin_launch(shell, arg, status);
+	ft_builtin_launch(shell, argv, status);
 	return (status);
 }
