053d061bd3e832c7794e1417601a948eb534eb13
jmartini <jacopomartini89@gmail.com>
2021-10-02 02:40:31 +0200

    built-in : export working

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -1,29 +1,99 @@
 #include "minishell.h"
 
-int	ft_export_arg_validity(char *arg)
+static void	ft_export_change(t_shell *shell, char *arg, char *env_name)
 {
-	//
+	char	*tmp;
+	int		len;
+	int		i;
+	int		env_name_len;
+
+	len = 0;
+	while (shell->env[len])
+		len++;
+	env_name_len = ft_strlen(env_name);
+	i = -1;
+	while (++i < len)
+	{
+		if (!ft_memcmp(env_name, shell->env[i], env_name_len)
+			&& shell->env[i][env_name_len] == '=')
+		{
+			tmp = shell->env[i];
+			shell->env[i] = arg;
+			free (tmp);
+		}
+	}
+}
+
+static void	ft_export_add(t_shell *shell, char *arg)
+{
+	char	**tmp;
+	int		len;
+	int		i;
+
+	tmp = shell->env;
+	len = 0;
+	while (tmp[len])
+		len++;
+	shell->env = ft_calloc(len + 2, sizeof(*shell->env));
+	i = -1;
+	while (++i < len)
+	{
+		shell->env[i] = ft_strdup(tmp[i]);
+		if (!shell->env[i])
+			ft_perror_exit(ERR_SYS_MALLOC);
+	}
+	shell->env[len] = arg;
+	ft_gc_env(tmp);
+}
+
+static void	ft_export_stuff(t_shell *shell, char *arg)
+{
+	char	*env;
+	char	*env_name;
+	char	*arg_heap;
+	int		i;
+
+	env = NULL;
+	env_name = NULL;
+	arg_heap = NULL;
+	i = 0;
+	while (arg[i])
+	{
+		i++;
+		if (arg[i] == '=')
+		{
+			env_name = ft_calloc(i, sizeof(*env_name));
+			if (!env_name)
+				ft_perror_exit(ERR_SYS_MALLOC);
+			ft_memcpy(env_name, arg, i);
+			env = ft_getenv(shell, env_name);
+			arg_heap = ft_strdup(arg);
+			if (!arg_heap)
+				ft_perror_exit(ERR_SYS_MALLOC);
+			if (!env)
+				ft_export_add(shell, arg_heap);
+			else
+				ft_export_change(shell, arg_heap, env_name);
+			free (env_name);
+		}
+	}
 }
 
 void	ft_export(t_shell *shell, char **argv)
 {
-	int		argc;
 	char	*tmp;
+	int		i;
 
-	argc = 0;
-	while (argv[argc])
-		argc++;
-	if (argc == 1)
+	i = 1;
+	while (argv[i])
+	{
+		ft_export_stuff(shell, argv[i]);
+		i++;
+	}
+	if (i == 1)
 	{
 		ft_env(shell, NULL);
 		return ;
 	}
-	else
-	{
-		if (!ft_export_arg_validity(argv[1]))
-			/* add */;
-		else
-			/* getenv */;
-		ft_printf("WIP\n");
-	}
+	ft_env_return(shell, 0);
 }
