c11f3bff22ae71a077b30e2ba66892326f814d30
Jacopo Martini <jacopomartini89@gmail.com>
2021-10-28 02:48:25 +0200

    header fancy

--- src/utils/header.c
+++ src/utils/header.c
@@ -3,9 +3,15 @@
 void
 	ft_header(void)
 {
-	ft_printf("\e[1m42 Minishell\e[0m \e[3mas beautiful as a shell\e[0m\n");
+	ft_printf("*\n");
+	ft_printf("\e[1m42 Minishell\e[0m\n\e[3mas beautiful as a shell\e[0m\n");
+	ft_printf("\n");
 	ft_printf("Emulating Bash Version 5.1\n");
-	ft_printf("Bash Reference Manual ");
+	ft_printf("Bash Reference Manual:\n");
 	ft_printf("<http://www.gnu.org/software/bash/>\n");
+	ft_printf("Minishell Reference Manual:\n");
+	ft_printf("<http://github.com/jmartini89/42_minishell/wiki>\n");
+	ft_printf("\n");
 	ft_printf("Made by jmartini & mcrisari\n");
+	ft_printf("*\n");
 }
