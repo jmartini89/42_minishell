423314341ce9867d9f38da604f5d5a9e1aa9da5d
Jacopo Martini <jacopomartini89@gmail.com>
2021-11-08 22:59:32 +0100

    updated headers' header + exec wip

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -12,15 +12,40 @@
 
 #include "minishell.h"
 
+static void
+	ft_child(t_shell *shell, int i)
+{
+	int	builtin;
+	int	err;
+
+	ft_signal_default();
+	builtin = ft_builtin_check(shell->cmd[i].argv);
+	if (builtin)
+		ft_builtin_launch(shell, shell->cmd[i].argv, builtin, TRUE);
+	else if (!ft_is_path(shell->cmd[i].argv[0]))
+		if (!ft_exec_env_path(shell, &shell->cmd[i].argv[0]))
+			ft_error_exit(ERR_EXEC_NOCMD, NULL, 127);
+	if (execve(shell->cmd[i].argv[0], shell->cmd[i].argv, shell->env) == -1)
+	{
+		err = errno;
+		rl_clear_history();
+		if (err == ENOENT)
+			ft_error_exit(ERR_EXEC_NOFILE, NULL, 127);
+		if (err == EACCES)
+			ft_error_exit(ERR_EXEC_PERM, NULL, 126);
+		else
+			ft_error_exit(ERR_EXEC_UNKWN, NULL, EXIT_FAILURE);
+	}
+}
+
 void
 	ft_exec(t_shell *shell)
 {
 	int		builtin;
-	int		pid;
-	// int		*pid_arr;
+	pid_t	pid;
+	pid_t	*pid_arr;
 	int		wstatus;
 	int		wexit;
-	int		err;
 	int		i;
 
 	builtin = ft_builtin_check(shell->cmd[0].argv);
@@ -32,51 +57,45 @@ void
 		return ;
 	}
 
+	pid_arr = ft_calloc(shell->cmd_cnt, sizeof(*pid_arr));
+	if (pid_arr == NULL)
+		ft_error_exit(errno, "malloc", EXIT_FAILURE);
+
 	i = 0;
 	while (i < shell->cmd_cnt)
 	{
+		if ((i + 1) < shell->cmd_cnt)
+			printf("PIPE R DETECTED\n");
+		if (i)
+			printf("PIPE L DETECTED\n");
 		if (shell->cmd[i].argv)
 		{
 			pid = fork();
 			if (pid < 0)
 				ft_error_exit(errno, "fork", EXIT_FAILURE);
-			if (!pid)
-			{
-				ft_signal_default();
-				builtin = ft_builtin_check(shell->cmd[i].argv);
-				if (builtin)
-					ft_builtin_launch(shell, shell->cmd[i].argv, builtin, TRUE);
-				else if (!ft_is_path(shell->cmd[i].argv[0]))
-					if (!ft_exec_env_path(shell, &shell->cmd[i].argv[0]))
-						ft_error_exit(ERR_EXEC_NOCMD, NULL, 127);
-				if (execve(shell->cmd[i].argv[0], shell->cmd[i].argv, shell->env) == -1)
-				{
-					err = errno;
-					rl_clear_history();
-					if (err == ENOENT)
-						ft_error_exit(ERR_EXEC_NOFILE, NULL, 127);
-					if (err == EACCES)
-						ft_error_exit(ERR_EXEC_PERM, NULL, 126);
-					else
-						ft_error_exit(ERR_EXEC_UNKWN, NULL, EXIT_FAILURE);
-				}
-			}
+			if (pid == 0)
+				ft_child(shell, i);
 			else
-			{
-				wexit = waitpid(pid, &wstatus, WUNTRACED);
-				if (wexit == -1)
-					ft_error_exit(errno, "waitpid", EXIT_FAILURE);
-				if (WIFSTOPPED(wstatus))
-					ft_env_return(shell, WSTOPSIG(wstatus) + 128);
-				if (WIFSIGNALED(wstatus))
-				{
-					ft_printf("\n");
-					ft_env_return(shell, WTERMSIG(wstatus) + 128);
-				}
-				if (WIFEXITED(wstatus))
-					ft_env_return(shell, WEXITSTATUS(wstatus));
-			}
+				pid_arr[i] = pid;
 		}
 		i++;
 	}
+	i = 0;
+	while (i < shell->cmd_cnt)
+	{
+		wexit = waitpid(pid_arr[i], &wstatus, WUNTRACED);
+		if (wexit == -1)
+			ft_error_exit(errno, "waitpid", EXIT_FAILURE);
+		if (WIFSTOPPED(wstatus))
+			ft_env_return(shell, WSTOPSIG(wstatus) + 128);
+		if (WIFSIGNALED(wstatus))
+		{
+			ft_printf("\n");
+			ft_env_return(shell, WTERMSIG(wstatus) + 128);
+		}
+		if (WIFEXITED(wstatus))
+			ft_env_return(shell, WEXITSTATUS(wstatus));
+		i++;
+	}
+	free (pid_arr);
 }
