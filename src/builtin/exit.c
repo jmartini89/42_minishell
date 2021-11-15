e2b84bda8b91b4470d8c77fc6ee8ec807e1a268e
Jacopo Martini <jacopomartini89@gmail.com>
2021-11-15 04:01:48 +0100

    exit args

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -12,21 +12,40 @@
 
 #include "minishell.h"
 
+static int
+	ft_is_numeric(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (ft_isdigit(arg[i]) == FALSE
+			&& arg[i] != '+'
+			&& arg[i] != '-')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
 void
 	ft_exit(t_shell *shell, char **argv, int process)
 {
 	int	argc;
+	int	status;
 
+	status = ft_atoi(shell->ret_str);
+	if (process == FALSE)
+		ft_printf("exit\n");
 	argc = ft_argc(argv);
 	if (argc > 1)
 	{
-		ft_error(ERR_BLTIN_ARGS, NULL);
-		ft_env_return(shell, 1);
-		if (process)
-			exit (ft_atoi(shell->ret_str));
-		return ;
+		if (ft_is_numeric(argv[1]) == FALSE)
+			ft_error_exit(ERR_BLTIN_EXIT_NUM, NULL, 2);
+		if (argc > 2)
+			ft_error_exit(ERR_BLTIN_EXIT_ARGS, NULL, 1);
+		status = ft_atoi(argv[1]) & 0xFF;
 	}
-	if (process == FALSE)
-		ft_printf("exit\n");
-	exit (ft_atoi(shell->ret_str));
+	exit (status);
 }
